8d9bfc32c975a7fc8d0633b31b0f95e7b79466c0
YJ402 <uky0916@gmail.com>
2025-02-06 21:12:47 +0900

    [feat] GameView

---
+++ MarvelHeroes/YJ_Scene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelHeroes
+{
+    public abstract class Scene
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //ViewNornalScene 사용법 예시
+        //public Scene()
+        //{
+        //for (int i = 0; i<GameView.ViewNormalScene.Count; i++)
+        //    {
+        //        GameView.ViewNormalScene[i].Invoke(this);
+    }
+    //}
+}
+}
