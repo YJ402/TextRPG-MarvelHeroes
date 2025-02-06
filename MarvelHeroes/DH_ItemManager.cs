81d58b8370e4610402ef8535e44b406c23348b16
MyaMya0709 <chlehdgh96@gmail.com>
2025-02-06 21:16:55 +0900

    [feat] ItemManager.cs에 코드추가
    아이템 인터페이스 추가
    장착아이템 클래스 추가
    사용아이템 작성 중

--- MarvelHeroes/DH_ItemManager.cs
+++ MarvelHeroes/DH_ItemManager.cs
@@ -6,7 +6,76 @@ using System.Threading.Tasks;
 
 namespace MarvelHeroes
 {
+    
+
     internal class DH_ItemManager
     {
+        
     }
+
+    public enum ItemType
+    {
+        Weapon,
+        Amor,
+        Healing
+    }
+
+    public interface Item
+    {
+        public string Name { get; }
+        public int Value { get; }
+        public string Descrip { get; }
+        public int Cost { get; }
+    }
+
+    public class EquipItem : Item
+    {
+        public string Name { get; }
+        public ItemType Type { get; }
+        public int Value { get; }
+        public string Descrip { get; }
+        public int Cost { get; }
+        public bool IsPurchase { get; set; }
+        public bool IsEquip { get; set; }
+
+        public EquipItem(string name, ItemType type, int value, string descrip, int cost)
+        {
+            Name = name;
+            Type = type;
+            Value = value;
+            Descrip = descrip;
+            Cost = cost;
+            IsPurchase = false;
+            IsEquip = false;
+        }
+        public string ItemDisplay()
+        {
+            string str = IsEquip ? "[E]" : "";
+            str += $"{Name} | {GetTypeString()} | {Descrip}";
+            return str;
+        }
+
+        public string GetTypeString()
+        {
+            string str = (Type == ItemType.Weapon ? $"공격력 +{Value}" : $"방어력 +{Value}");
+            return str;
+        }
+
+        public string GetPriceString()
+        {
+            string str = IsPurchase ? "구매완료" : $"{Cost}";
+            return str;
+        }
+    }
+
+    public class UsingItem
+    {
+        public string Name { get; }
+        public ItemType Type { get; }
+        public int Value { get; }
+        public string Descrip { get; }
+        public int Cost { get; }
+    }
+
+
 }
