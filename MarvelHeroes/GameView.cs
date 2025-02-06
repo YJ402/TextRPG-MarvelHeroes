274cb381ffc648107c13939c5a7c58a16ccd575d
YJ402 <uky0916@gmail.com>
2025-02-06 17:07:37 +0900

    [feat] GameView 틀

---
+++ MarvelHeroes/GameView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelHeroes
+{
+    public static class GameView
+    {
+
+        //예시 PrintText("[탐험 결과]", 200, ConsoleColor.DarkBlue);
+        public static void PrintText(string text, int ms = 0, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Thread.Sleep(ms);
+        }
+
+
+        public static void ViewScene(Scene scene)
+        {
+            PrintText(scene, 0, ConsoleColor.Green);
+        }
+
+        public static void ViewSceneInfo(Scene scene)
+        {
+
+        }
+
+        public static void ViewSceneSelect(Scene scene)
+        {
+
+        }
+
+        public static bool ViewYesOrNo(Scene scene)
+        {
+
+        }
+    }
+}
