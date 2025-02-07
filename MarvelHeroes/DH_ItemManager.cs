5bf54b0d1a449759b7d1cdec2122fa13fe1e587a
MyaMya0709 <chlehdgh96@gmail.com>
2025-02-07 17:27:08 +0900

    [feat] ItemManager.cs의 코드 추가 및 수정
    - 아이템 인터페이스를 클레스로 수정
    - 수정 후 자식 클래스로 상속
    - 아이템 클래스를 추상클래스로 규정
    - Use함수를 자식클래스에서 용도에 맞게 변형

--- MarvelHeroes/DH_ItemManager.cs
+++ MarvelHeroes/DH_ItemManager.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,12 +32,13 @@ namespace MarvelHeroes
             //public UsingItem(string name, ItemType type, int value, string descrip, int cost)
             usingItems = new List<UsingItem>
             {
-                new UsingItem("소형 힐링 포션", ItemType.Healing, 20,"소량의 체력을 회복시켜줍니다.", 10),
-                new UsingItem("대형 힐링 포션", ItemType.Healing, 50,"대량의 체력을 회복시켜줍니다.", 30)
+                new UsingItem("소형 힐링 포션", ItemType.Healing, 20, "소량의 체력을 회복시켜줍니다.", 10, 2),
+                new UsingItem("대형 힐링 포션", ItemType.Healing, 50, "대량의 체력을 회복시켜줍니다.", 30, 1)
             };
         }
-    }
 
+    }
+    //직업종류의 상수
     public enum JobType
     {
         IronMan,
@@ -43,6 +47,7 @@ namespace MarvelHeroes
         Hulk
     }
 
+    //아이템 종류의 상수
     public enum ItemType
     {
         Weapon,
@@ -50,55 +55,110 @@ namespace MarvelHeroes
         Healing
     }
 
-    public interface Item
+    //아이템 클래스
+    public abstract class Item
     {
-        public string Name { get; }
-        public int Value { get; }
-        public string Descrip { get; }
-        public int Cost { get; }
-    }
+        public string Name { get; set; }
+        public ItemType ItemType { get; set; }
+        public int Value { get; set; }
+        public string Descrip { get; set; }
+        public int Cost { get; set; }
 
-    public class EquipItem : Item
-    {
-        public string Name { get; }
-        public JobType JobType { get; }
-        public ItemType ItemType { get; }
-        public int Value { get; }
-        public string Descrip { get; }
-        public int Cost { get; }
-        public bool IsPurchase { get; set; }
-        public bool IsEquip { get; set; }
-
-        public EquipItem(string name, ItemType itemtype, JobType jobType, int value, string descrip, int cost)
+        public Item(string name, ItemType itemtype, int value, string descrip, int cost)
         {
             Name = name;
             ItemType = itemtype;
-            JobType = jobType;
             Value = value;
             Descrip = descrip;
             Cost = cost;
+        }
+
+        public abstract void Use(BW_Player.player, BW_Job.jobType);
+
+    }
+
+    //장착아이템 클래스
+    public class EquipItem : Item
+    {
+        public JobType JobType { get; set; }
+        public bool IsPurchase { get; set; }
+        public bool IsEquip { get; set; }
+
+        //public Child(int X) : base(X) { } //base키워드를 이용하여 상속
+        public EquipItem(string name, ItemType itemtype, JobType jobType, int value, string descrip, int cost) : base(name, itemtype, value, descrip, cost)
+        {
+            JobType = jobType;
             IsPurchase = false;
             IsEquip = false;
         }
+
+        public override void Use(BW_Player.player, BW_Job.jobType)
+        {
+            //플레이어의 직업의 종류를 불러옴
+            BW_Player.Job playerjob = new BW_Player.Job;
+            //조건문if 사용해서 플래이어의 직업과 무기의 직업을 비교하는 조건
+            //착용 불가 메세지 후 return
+            if (playerjob != this.JobType)
+            {
+                Console.WriteLine("사용이 불가능한 장비입니다!");
+                return;
+            }
+            
+            if (this.IsEquip) //장착 중인 아이템 선택시 해제
+            {
+                this.IsEquip = false;
+
+                if (this.ItemType == ItemType.Weapon) { }
+                //EquipAtk -= this.Value;
+                else if (this.ItemType == ItemType.Amor) { }
+                //EquipDef -= this.Value;
+            }
+            else //아이템 착용
+            {
+                this.IsEquip = true;
+
+                if (this.ItemType == ItemType.Weapon) { } //타입이 무기일 경우 공격력 증가 
+                    //EquipAtk += this.Value;
+                else if (this.ItemType == ItemType.Amor) { } //타입이 갑옷일 경우 방어력 증가
+                    //EquipDef += this.Value;
+            }
+        }
+
     }
 
-    public class UsingItem
+    //소모아이템 클래스
+    public class UsingItem : Item
     {
-        public string Name { get; }
-        public ItemType Type { get; }
-        public int Value { get; }
-        public string Descrip { get; }
-        public int Cost { get; }
+        public int Quantity { get; set; }
 
-        public UsingItem(string name, ItemType type, int value, string descrip, int cost)
+        public UsingItem(string name, ItemType itemtype, int value, string descrip, int cost, int quantity) : base(name, itemtype, value, descrip, cost)
         {
-            Name = name;
-            Type = type;
-            Value = value;
-            Descrip = descrip;
-            Cost = cost;
+            Quantity = quantity;
+        }
+
+        public override void Use(BW_Player.player, BW_Job.jobType)
+        {
+            //플레이어의 체력을 불러옴
+            //조건문if 사용해서 포션 수량을 비교하는 조건
+            //사용 불가 메세지 후 return
+            if (this.Quantity <= 0)
+            {
+                Console.WriteLine($"{Name}이(가) 없습니다.");
+                return;
+            }
+
+            //아이템타입이 힐링일경우 회복
+            if (ItemType == ItemType.Healing)
+            {
+                player.Heal(Value);
+                Quantity--;
+                Console.WriteLine($"{player.Name}이(가) {Name}을 사용하였습니다.");
+                Console.WriteLine($"체력을 {Value}만큼 회복합니다.(남은 개수 : {Quantity})");
+            }
+            else
+            {
+                Console.WriteLine($"{Name}은(는) 사용할 수 없는 아이템입니다.")
+            }
         }
     }
-
-
 }
