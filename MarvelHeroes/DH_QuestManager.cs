2a0f42a509168b0127de194612cb5339b6a4c72e
MyaMya0709 <chlehdgh96@gmail.com>
2025-02-07 22:02:42 +0900

    [feat] QuestManager.cs 코드 추가
    - 퀘스트 수락 메서드 추가

--- MarvelHeroes/DH_QuestManager.cs
+++ MarvelHeroes/DH_QuestManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,26 +9,39 @@ namespace MarvelHeroes
 {
     internal class DH_QuestManager
     {
-        List<Quest> questlist;
-        List<Quest> acceptQuest;
+        List<Quest> questlist; //모든퀘스트 목록
+        List<Quest> acceptQuest; //플레이어가 받은 퀘스트
 
         public DH_QuestManager()
         {
             questlist = new List<Quest>
             {
-                new Quest ("이장의 부탁1","이장이 당신에게 근처 치타우리의 처리를 부탁했습니다.", 0, 101),
-                new Quest ("이장의 부탁2","이장이 당신에게 근처 치타우리의 처리를 부탁했습니다.", 0, 102),
-                new Quest ("이장의 부탁3","이장이 당신에게 근처 치타우리의 처리를 부탁했습니다.", 0, 103)
+                new HuntQuest ("이장의 부탁1","이장이 당신에게 근처 치타우리의 처리를 부탁했습니다.", 0, 101),
+                new HuntQuest ("이장의 부탁2","이장이 당신에게 근처 치타우리의 처리를 부탁했습니다.", 0, 102),
+                new HuntQuest ("이장의 부탁3","이장이 당신에게 근처 치타우리의 처리를 부탁했습니다.", 0, 103)
             };
 
-            acceptQuest = new List<Quest>();
+            acceptQuest = new List<Quest>(); //리스트 초기화
         }
 
         public void AcceptQuest(Quest quest)
         {
-            //Contain()함수는 
-            if (!acceptQuest.Contains(quest);
+            //public bool Contains (T item);
+            //Contain()함수는 item은 List<T>에서 찾을 객체(또는 값) 이며,
+            //item이 클래스의 객체 또는 배열과 같은 참조 타입에 대해서 값은 null이 될 수 있습니다.
+            //item이 존재하면 Contain() 함수는 True를 반환하고 그렇지 않으면 False를 반환합니다.
+            if (!acceptQuest.Contains(quest)) //이미 받은 퀘스트가 아닐때
+            {
+                acceptQuest.Add(quest);//퀘스트 추가
+                Console.WriteLine($"[퀘스트 수락] {quest.Name} 퀘스트를 받았습니다.");
+            }
+            else
+            {
+                Console.WriteLine("이미 받은 퀘스트입니다.");
+            }
         }
+
+        
 
     }
 
