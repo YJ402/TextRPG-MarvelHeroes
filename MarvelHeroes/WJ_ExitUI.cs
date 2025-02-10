63b68ca0b35939830081000d8191da34daa98365
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-02-10 10:05:01 +0900

    <Feat>ExitUI
    1. 종료구현

--- MarvelHeroes/WJ_ExitUI.cs
+++ MarvelHeroes/WJ_ExitUI.cs
@@ -9,5 +9,35 @@ namespace MarvelHeroes
 {
     internal class WJ_ExitUI
     {
+        public int ExitScene(int temp)
+        {
+            int sceneTemp = temp;
+            Console.Clear();
+            GameView.PrintText("게임종료", 0, ConsoleColor.Magenta);
+            Console.WriteLine();
+            Console.WriteLine("게임이 종료됩니다.");
+            Console.WriteLine();
+            Console.WriteLine("종료 하시겠습니까?");
+            Console.WriteLine();
+            Console.WriteLine("1. 예");
+            Console.WriteLine("0. 아니오");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요");
+            Console.Write(">>");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    if (choice == 0) { break; }
+                    else if (choice == 1) { Environment.Exit(0); }
+                }
+                else
+                {
+                    Console.WriteLine("입력을 다시시도해 주세요");
+                }
+            }
+            return sceneTemp;
+        }
     }
 }
