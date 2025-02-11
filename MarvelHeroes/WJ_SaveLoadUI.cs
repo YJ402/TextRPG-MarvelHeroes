378015ec61af1f59074865189738e43b49262132
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-02-11 09:07:55 +0900

    <chore>while문 변경

--- MarvelHeroes/WJ_SaveLoadUI.cs
+++ MarvelHeroes/WJ_SaveLoadUI.cs
@@ -11,25 +11,26 @@ namespace MarvelHeroes
     {
         public void SaveLoadScene()
         {
-            Console.Clear();
-            GameView.PrintText("게임 저장/불러오기", 0, ConsoleColor.Magenta);
-            Console.WriteLine();
-            Console.WriteLine("현재 진행 상황이 저장/불러오기 됩니다.");
-            Console.WriteLine();
-            Console.WriteLine("게임을 저장/불러오기 하기겠습니까?");
-            Console.WriteLine();
-            Console.WriteLine("1. 저장");
-            Console.WriteLine("2. 불러오기");
-            Console.WriteLine("0. 아니오");
-            Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요");
-            Console.Write(">>");
             while (true)
             {
+                Console.Clear();
+                GameView.PrintText("게임 저장/불러오기", 0, ConsoleColor.Magenta);
+                Console.WriteLine();
+                Console.WriteLine("현재 진행 상황이 저장/불러오기 됩니다.");
+                Console.WriteLine();
+                Console.WriteLine("게임을 저장/불러오기 하기겠습니까?");
+                Console.WriteLine();
+                Console.WriteLine("1. 저장");
+                Console.WriteLine("2. 불러오기");
+                Console.WriteLine("0. 아니오");
+                Console.WriteLine();
+                Console.WriteLine("원하시는 행동을 입력해주세요");
+                Console.Write(">>");
+
                 if (int.TryParse(Console.ReadLine(), out int choice))
                 {
                     if (choice == 0) { break; }
-                    else if (choice == 1) { ; }
+                    else if (choice == 1) {; }
                 }
                 else
                 {
