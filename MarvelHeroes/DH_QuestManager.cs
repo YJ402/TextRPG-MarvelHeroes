12b8825ba9eeb4d926718e5a51cd6044eadceaeb
MyaMya0709 <chlehdgh96@gmail.com>
2025-02-11 19:01:13 +0900

    Update DH_QuestManager.cs

--- MarvelHeroes/DH_QuestManager.cs
+++ MarvelHeroes/DH_QuestManager.cs
@@ -10,28 +10,82 @@ namespace MarvelHeroes
     public class QuestManager
     {
         public static List<Quest> questlist_Chief; //모든퀘스트 목록
-        public static List<Quest> questlist_Invagation; //모든퀘스트 목록
-        List<Quest> acceptQuest; //플레이어가 받은 퀘스트
+        public static List<Quest> questlist_Investigation; //모든퀘스트 목록
+        public List<Quest> acceptQuest; //플레이어가 받은 퀘스트
 
 
         public QuestManager()
         {
             questlist_Chief = new List<Quest>
             {
-            new HuntQuest("시장의 토벌 의뢰", "시장이 당신에게 근처 치타우리족의 처리를 부탁했습니다.", 3, 1101, "Goblin"),
-            new ItemQuest("영웅의 품격", "사장의 신뢰를 위해 방어구를 입은 멋진 모습을 보여줍시다. ", 1, 1201,ItemType.Amor),
+            new HuntQuest("시장의 토벌 의뢰", "시장이 당신에게 근처 치타우리족의 처리를 부탁했습니다.\n치타우리 3마리를 처리합시다.", 3, 1101, "Goblin"),
+            new EquipQuest("영웅의 품격", "사장의 신뢰를 위해 방어구를 입은 멋진 모습을 보여줍시다. ", 1, 1201,ItemType.Amor),
             new LevelQuest("시장의 신뢰", "시장은 강한 자를 원합니다. 시장의 신뢰를 위해 레벨 10을 달성하세요.", 10, 1301)
             };
 
-            questlist_Invagation = new List<Quest>
+            questlist_Investigation = new List<Quest>
             {
             new HuntQuest("미망인의 복수", "미망인의 남편을 죽인 치타우리를 사냥해옵시다.", 3, 2101, "Goblin"),
-            new ItemQuest("전투준비", "무기를 장착하여 전투에 대비합시다.", 1, 2201, ItemType.Weapon),
+            new EquipQuest("전투준비", "무기를 장착하여 전투에 대비합시다.", 1, 2201, ItemType.Weapon),
             new LevelQuest("부족한 힘", "적에 비해 당신은 약합니다. 레벨 15을 달성합시다.", 15, 2301)
             };
 
             acceptQuest = new List<Quest>(); //리스트 초기화
         }
+
+        public void MainQueststart(string questTrigger)
+        {
+            List<Quest> list = new List<Quest>();
+
+            if (questTrigger == "Chief")
+            {
+                list = questlist_Chief;
+            }
+            else if(questTrigger == "Investigation")
+            {
+                list = questlist_Investigation;
+            }
+            Console.WriteLine("메인 퀘스트");
+            Console.WriteLine("[퀘스트 목록]");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {list[i].Name} | {list[i].Descrip}");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("어떤 퀘스트를 수락하시겠습니까?");
+            Console.WriteLine("");
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int sellecNumber;
+                bool isNumber = int.TryParse(input, out sellecNumber);
+                if (isNumber == false || list.Count <= sellecNumber)
+                {
+
+                    Console.WriteLine("잘못된 입력입니다");
+                    continue;
+                }
+                if (sellecNumber == 0)
+                {
+                    GameManager.Instance.SM.ChangeCurrentScene(SceneNum.Town);
+                    break;
+
+                }
+
+                AcceptQuest(list[sellecNumber - 1]);
+                break;
+
+            }
+        }
+
+
+
+
 
         public void AcceptQuest(Quest quest)
         {
@@ -50,12 +104,12 @@ namespace MarvelHeroes
             }
         }
 
-        public void CheckCompleteQuest(List<Monster> killMonster, Player player, EquipItem item)
+        public void CheckCompleteQuest(List<Monster> Monster, Player player, EquipItem item)
         {
             List<Quest> completedQuests = new List<Quest>();
             foreach (Quest quest in acceptQuest)
             {
-                if (quest.IsCompleted(killMonster, player,item))
+                if (quest.IsCompleted(Monster, player,item))
                 {
                     completedQuests.Add(quest);
                     acceptQuest.Remove(quest);
@@ -104,12 +158,12 @@ namespace MarvelHeroes
         public abstract void Questclear();
     }
 
-    public class ItemQuest : Quest
+    public class EquipQuest : Quest
     {
 
         public ItemType RequiredType { get; set; } // 장착해야 할 아이템 타입
 
-        public ItemQuest(string name, string descrip, int demand, int questId, ItemType requiredType)
+        public EquipQuest(string name, string descrip, int demand, int questId, ItemType requiredType)
             : base(name, descrip, demand, questId)
         {
             RequiredType = requiredType;
@@ -140,15 +194,15 @@ namespace MarvelHeroes
 
     public class HuntQuest : Quest
     {
-        string targetMonster;
+        private string targetMonster;
         public HuntQuest(string name, string descrip, int demand, int questId, string monster)
             : base(name, descrip, demand, questId)
         {
             targetMonster = monster; 
         }
-        public override bool IsCompleted(List<Monster> killMonster, Player player, EquipItem item)
+        public override bool IsCompleted(List<Monster> Monster, Player player, EquipItem item)
         {
-            foreach (Monster m in killMonster)
+            foreach (Monster m in Monster)
             {
                 if (m.MonsterName == targetMonster)
                     Demand--;
